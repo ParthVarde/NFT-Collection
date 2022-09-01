f5c129c485b2ed5c112e564b624d6b712b4b101a
Parth <parth.varde@esparkbizmail.com>
2022-09-01 09:59:25 +0530

    Redeployed contract

--- hardhat-module/constants/index.js
+++ hardhat-module/constants/index.js
@@ -1,4 +1,4 @@
-const WHITELIST_CONTRACT_ADDRESS = "0xd53710cb7aaB4f1Ac871eD8A032582b8F03f34AA";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const WHITELIST_CONTRACT_ADDRESS = "0xad1dE0B8f47ef5bE18e1c735F51B59a82c8bA47F";
+const METADATA_URL = "https://nft-collection-beta-olive.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
